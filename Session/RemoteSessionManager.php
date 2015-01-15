ab8fcafd5e8142a5cf8b6ad77a2319cc999c3c5e
Alexandr Viniychuk <mounters@gmail.com>
2015-01-15 17:00:00 +0200

    added compatibility for php 5.3 with getallheaders() disabled

--- Session/RemoteSessionManager.php
+++ Session/RemoteSessionManager.php
@@ -37,3 +37,19 @@ class RemoteSessionManager {
             )
         );
     }}
+
+if (!function_exists('getallheaders'))
+{
+    function getallheaders()
+    {
+        $headers = '';
+        foreach ($_SERVER as $name => $value)
+        {
+            if (substr($name, 0, 5) == 'HTTP_')
+            {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
