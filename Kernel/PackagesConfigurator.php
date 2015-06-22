e629012fd772285194db9adcd32f7768c5bb4b79
Alexandr Viniychuk <a@viniychuk.com>
2015-06-22 18:41:22 -0400

    package and panel

--- Kernel/PackagesConfigurator.php
+++ Kernel/PackagesConfigurator.php
@@ -15,6 +15,7 @@ use Solve\Database\DatabaseService;
 use Solve\Database\Models\Abilities\FilesAbility;
 use Solve\Database\Models\ModelOperator;
 use Solve\EventDispatcher\BaseEvent;
+use Solve\Http\Request;
 
 class PackagesConfigurator {
 
@@ -68,6 +69,40 @@ class PackagesConfigurator {
                 'listener' => array($this, 'onEnvironmentUpdate')
             )
         );
+    }
+
+    public function __destruct() {
+        if (DC::getRouter()->getCurrentRequest()->getMethod() == Request::MODE_CONSOLE) return true;
+        if (DC::getProjectConfig('dev/toolbar') != true) return true;
+        $routeName = DC::getRouter()->getCurrentRoute()->getName();
+        $css =<<<TEXT
+.__solve_debug_panel {
+    z-index: 1030;
+    text-align: left;
+    font-size: 13px;
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 40px;
+    padding: 10px;
+    color: #666;
+    background-color: #FFF;
+    box-shadow: 0 0 0 1px #e0e0e0;
+    -webkit-box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.08), 0px 0px 0px 2px rgba(0, 0, 0, 0.02);
+    box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.08), 0px 0px 0px 2px rgba(0, 0, 0, 0.02);
+}
+TEXT;
+        $html =<<<TEXT
+<style>
+$css
+</style>
+<div class="__solve_debug_panel">
+    route: <b>$routeName</b>
+</div>
+TEXT;
+        echo $html;
+
     }
 
 }
